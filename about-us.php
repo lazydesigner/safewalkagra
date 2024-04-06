2c1df76f027ffacb40681a634365c49eac7b46de
deepak <deepakbaradwaj933@gmail.com>
2024-04-06 16:17:18 +0530

    initial commit

--- about-us.php
+++ about-us.php
@@ -22,7 +22,26 @@
     <meta name="twitter:description" content="We offer top class Agra escorts at cash payment. All type of escorts available like Housewife, college girls, Russian models, etc" />
     <link rel="canonical" href="<?= get_url() ?>about-us.html" />
     <!-- apart -->
+    <link rel="stylesheet" href="<?= get_url() ?>css/index.css">
     <link rel="stylesheet" href="<?= get_url() ?>css/navbar.css">
+    <style>
+         .content-section .content-area a {
+            font-weight: bold;
+            color: var(--link-color);
+        }
+
+        .content-section .container p {
+            font-size: 1.1rem;
+            line-height: 30px;
+        }
+
+        .content-section .container ul li {
+            font-size: 1.1rem;
+            margin: 1% 0;
+        }
+        .content-area-img{padding: 2%;}
+        .content-area-flex{align-items: center;}
+    </style>
     <?=$schema ?>
 </head>
 
@@ -30,7 +49,7 @@
     <?php include './navbar.php' ?>
     <div class="safewalkheading">
         <div class="video-below-text">
-            <div class="below-text">
+            <div class="below-text"> 
                 <h1>About Us</h1>
                 <p>Call Us For further details</p>
             </div>
@@ -39,6 +58,84 @@
             </div>
         </div>
     </div>
+    <section class="content-section">
+        <div class="content-area-flex container">
+            <div class="content-area">
+                <h2>Explore the beauty of the Taj City to walk with Agra Escort</h2>
+                <p>Have you completed the goal of visiting the Taj Mahal? What will people do for their perfection? Staying in a monotone vibe is bad, and one should take the wise step to break from the unwanted condition. So, you ought to transition from the worse to better conditions to step into the valuable escort agency in Agra. Here, you can find mature and hot girls to love you more.  As a result, you cannot be sad anymore, and these escorts are the reasons for your respectable smile. This escort becomes the real partner to spend quality time with you. </p>
+
+                <p>They never keep in you the dubious condition, and tell you how long she can intimate with you. By the way, you can recall them anywhere and anytime. They become ready for your causal as per your preference. Taking the service of this escort is a wise decision for you if you want to add prosperity to your life.  An individual becomes quite confident as they have the proper time to get the soft touch.  They are the perfect option to gain the coupling experience during the day. </p>
+                <p>Their undescribed sensual touch you relish as long as you can. Do not think that you see the bad thinking in your mind. But, they have the same level to interact with your body and soul. There is no way that this busty personality refuses your erotic urges. They have the craziness to spend some meaningful time with you.
+</p>
+            </div>
+            <div class="content-area">
+                <div class="content-area-img">
+                <img src="<?=get_url() ?>images/14.png" width="100%" height="100%" loading="lazy" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content-section">
+        <div class="content-area-flex container">
+            <div class="content-area">
+                <h2>Get the mental calmness to the company of the busty girl in your area</h2>
+                <p>Do you want to break from the sadness? But, you do not know the best hack to do so. Why do not try to begin your day with a beautiful escort? They are talented enough to impress you. So, you do not mess up with the different chaos of your life and make your mind to something best with them. Pertain all actions in such a way that you never say to take the sensual massage for a particular time. Our Independent Agra escorts have the high affirmation to spend time with you. </p>
+
+                <p>They express their wish to do outdoor sex as well. Anyway, you are never fed up with our service as we are well-behaved and like to worship more for more enjoyment. With the collaboration of our horny Agra escort, you can charm your mind to give a real boost to your expectations. The overall thinking is our Agar escort will provide you the sensual service at an affordable price. But, it is not ill wish to such your hard-earned money in exchange for our sexual service. </p>
+                <p>We know this concern that making your part in the execution of the libido choice is not in your hands. By the way, you are not upset about how they can provide you with the most loving service. Our professionals in Agra offer you a rewarding experience of mental satisfaction.	</p>
+            </div>
+            <div class="content-area">
+                <div class="content-area-img">
+                <img src="<?=get_url() ?>images/18.png" width="100%" height="100%" loading="lazy" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content-section">
+        <div class="content-area-flex container">
+            <div class="content-area">
+                <h3>Be positive in the erotic vibes of lovely escort</h3>
+                <p>Even though approach our escort corridor in Agra, you are unable to figure out the select the best one. Happening of this incident takes place if you take this service first time. The main reason behind this because you cannot determine what attire improvement and adult practice are applicable to achieve the best result. The main thing is that they provide an exponential chance to mitigate with this service. Taking the escort service is the realistic approach they do not set a high price for this seducing and massage service.  </p>
+
+                <p>It means that a maximum person can achieve the sensual benefits. If some suspicion is going in your mind, then you can ask the recommended person how much she takes time to bring you into the seducing stage.  According to the description of your friend's suggestion, you can find a wonderful shot with them. That’s why you do not churn out your mind to take their service or not. </p>
+                <p>No matter what charge they get in the exchange of the escort service, they do not compromise the way of getting the gorgeous looking. Doing work on their physical appearance is their duty so that more and more customers come close to them. They will sit beside you to get the obvious stimulation and improve the hormonal inflow. Their short-term company inspires you more to make the fun enjoyable.</p>
+            </div>
+            <div class="content-area">
+                <div class="content-area-img">
+                <img src="<?=get_url() ?>images/4.png" width="100%" height="100%" loading="lazy" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content-section">
+        <div class="content-area-flex container">
+            <div class="content-area">
+                <h2>Capture the faun slots with the Russian escort to do high devotion in the sensual process</h2>
+                <p> When you are looking forward to the maximum input for drenching your inner soul, we have the specific sexual puppet to proffer you the loving experience. Our escort agency hosts beautiful and certified escorts who are ready to provide the service from scratch to the final stage. So, you do not need to draw an extra strategy on how to achieve the entire day's happiness.  Even though they are extra sensitive, they never dare to override your expectation. </p>
+
+                <p>They come in the glamorous to fee the toughest desire of their customer. It does not matter how much extent fulfilling this desire is easy for them. If you have the too much anxiety, then you must come with the Russian escorts.  The erotic vibes of the Russian escort is far better than other escort as they come ahead to understand to what the customer need on the high priority.</p>
+            </div>
+            <div class="content-area">
+                <div class="content-area-img">
+                <img src="<?=get_url() ?>images/17.png" width="100%" height="100%" loading="lazy" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content-section">
+        <div class="content-area-flex container">
+            <div class="content-area">
+                <h3>Spice up your glamorous night with Agra escort</h3>
+                <p>Pausing your human tendency to approach near the worthy and reliable escort is on top priority as you well the real appearance of the fantasy and excitement. If you come in the outskirts of the escort service, then you do not escape from the determination to achieve something better. By the way, no person come here for just shaking hand with the hot and sexy. They have to do the exceptional quality cumming experience whose craving lies in many men’s mind. If you are thinking about to gain the all marginal benefits related to causal relationship, then you can consider our service 		
+</p>
+            </div>
+            <div class="content-area">
+                <div class="content-area-img">
+                <img src="<?=get_url() ?>images/11.png" width="100%" height="100%" loading="lazy" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
     <?php include './footer.php' ?>
 </body>
 
