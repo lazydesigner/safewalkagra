892f70f4f4018dfef760dc9245ab307b1fdcfca4
deepak <deepakbaradwaj933@gmail.com>
2024-04-06 16:39:25 +0530

    initial commit

--- common.php
+++ common.php
@@ -9,7 +9,8 @@ $noindex = '<meta name="robots" content="noindex, nofollow, max-snippet:-1, max-
     ';
 $pages_css = ' <link rel="stylesheet" href="'.get_url().'assets/css/header.css">
 <link rel="stylesheet" href="'.get_url().'assets/css/pages.css">
-<link rel="stylesheet" href="'.get_url().'assets/css/footer.css?v=1">';
+<link rel="stylesheet" href="'.get_url().'assets/css/footer.css?v=1">
+<meta name="google-site-verification" content="Um_16scLmoRytgR1d4A14aOANdT3R0i9ZoT-ev2jSUo" />';
 
 $schema = '<script type="application/ld+json">
 {
